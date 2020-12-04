62eebd45e7fe170eb31268935beea0bfeda7f1b8
Anthony Coniglio <59713297+AnthonyConiglio@users.noreply.github.com>
2020-12-04 06:29:24 -0500

    Delete constants.php

--- config/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('EMAIL', 'cop4331noteswebsite2@gmail.com');
-define('PASSWORD', 'Anthony74!');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'user_logins');
